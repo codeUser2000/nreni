954d3f1162807bd82812994a22c26765ea9130fb
unmatched0 <hovhannisya03@mail.ru>
2022-12-05 16:13:06 +0400

    change images in Home.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
+import {Provider} from 'react-redux';
+import {applyMiddleware, compose, legacy_createStore as createStore} from 'redux';
 import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 import App from './App';
@@ -10,7 +10,7 @@ import reducers from './store/reducers';
 import sagas from './store/sagas';
 import 'react-toastify/dist/ReactToastify.css';
 import 'react-datepicker/dist/react-datepicker.css';
-import './assets/style.scss';
+import './assets/css/style.css';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -23,9 +23,9 @@ sagaMiddleware.run(sagas);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
 );
 
 reportWebVitals();
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,107 +1,113 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import {Helmet} from 'react-helmet';
 import Carousel from 'nuka-carousel';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import Wrapper from '../components/Wrapper';
 import MyCarouselComp from '../components/MyCarouselComp';
 import img from '../assets/img/post/banner.jpg';
+import ring from '../assets/img/post/ring.jpg';
+import chainRing from '../assets/img/post/chainRing.jpg';
+import necklace from '../assets/img/post/necklace.jpg';
+import nameNecklace from '../assets/img/post/nameNecklace.jpg';
+import earring from '../assets/img/post/earring.jpg';
+import bracelet from '../assets/img/post/bracelet.jpg';
 import NewProduct from '../components/NewProduct';
 import newProduct from '../newProduct';
 
 function Home() {
-  return (
-    <Wrapper>
-      <Helmet>
-        <title>NRENI</title>
-      </Helmet>
-      <div id="header-carousel" className="carousel slide" data-ride="carousel">
-        <div className="carousel-inner">
-          <Carousel
-            autoplay="true"
-            dragging="true"
-            pauseOnHover="true"
-            wrapAround="true"
-          >
-            <MyCarouselComp imgSrc={img} />
-            <MyCarouselComp imgSrc={img} />
-          </Carousel>
-        </div>
-        <main className="home">
-          <div className="container">
-            <div className="assortment">
-              <div className="assortmentColumn1">
-                <div className="assortmentColumnDesk">
-                  <figure className="assortmentItem">
-                    <img
-                      style={{ objectPosition: 'bottom' }}
-                      src={img}
-                      alt=""
-                      className="assortmentImg"
-                    />
-                  </figure>
+    return (
+        <Wrapper>
+            <Helmet>
+                <title>NRENI</title>
+            </Helmet>
+            <div id="header-carousel" className="carousel slide" data-ride="carousel">
+                <div className="carousel-inner">
+                    <Carousel
+                        autoplay="true"
+                        dragging="true"
+                        pauseOnHover="true"
+                        wrapAround="true"
+                    >
+                        <MyCarouselComp imgSrc={img}/>
+                        <MyCarouselComp imgSrc={img}/>
+                    </Carousel>
                 </div>
-                <div className="assortmentColumnDesk">
-                  <p className="assortmentInfo">Rings</p>
-                  <figure className="assortmentItem">
-                    <img src={img} alt="" className="assortmentImg" />
-                  </figure>
-                </div>
-              </div>
-              <div className="assortmentColumn2">
-                <div className="assortmentColumnDesk">
-                  <figure className="assortmentItem">
-                    <img
-                      style={{ objectPosition: 'bottom' }}
-                      src={img}
-                      alt=""
-                      className="assortmentImg"
-                    />
-                  </figure>
-                  <div className="assortmentDesk">
-                    <p className="assortmentInfo">Necklaces</p>
-                    <button type="button" className="assortmentBtn">
-                      <Link to="/shop" className="assortmentLink">Shop now</Link>
-                    </button>
-                  </div>
-                </div>
-                <div className="assortmentColumnDesk">
-                  <figure className="assortmentItem">
-                    <img src={img} alt="" className="assortmentImg" />
-                  </figure>
-                </div>
-              </div>
-              <div className="assortmentColumn3">
-                <div className="assortmentColumnDesk">
-                  <figure className="assortmentItem">
-                    <img src={img} alt="" className="assortmentImg" />
-                  </figure>
-                  <p className="assortmentInfo">
-                    Earrings
-                  </p>
-                </div>
-                <div className="assortmentColumnDesk">
-                  <figure className="assortmentItem">
-                    <img src={img} alt="" className="assortmentImg" />
-                  </figure>
-                  <p className="assortmentInfo">
-                    Bracelets
-                  </p>
-                </div>
-              </div>
+                <main className="home">
+                    <div className="container">
+                        <div className="assortment">
+                            <div className="assortmentColumn1">
+                                <div className="assortmentColumnDesk">
+                                    <figure className="assortmentItem">
+                                        <img
+                                            style={{objectPosition: 'bottom'}}
+                                            src={ring}
+                                            alt=""
+                                            className="assortmentImg"
+                                        />
+                                    </figure>
+                                </div>
+                                <div className="assortmentColumnDesk">
+                                    <p className="assortmentInfo">Rings</p>
+                                    <figure className="assortmentItem">
+                                        <img src={chainRing} alt="" className="assortmentImg"/>
+                                    </figure>
+                                </div>
+                            </div>
+                            <div className="assortmentColumn2">
+                                <div className="assortmentColumnDesk">
+                                    <figure className="assortmentItem">
+                                        <img
+                                            style={{objectPosition: 'bottom'}}
+                                            src={necklace}
+                                            alt=""
+                                            className="assortmentImg"
+                                        />
+                                    </figure>
+                                    <div className="assortmentDesk">
+                                        <p className="assortmentInfo">Necklaces</p>
+                                        <button type="button" className="assortmentBtn">
+                                            <Link to="/shop" className="assortmentLink">Shop now</Link>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className="assortmentColumnDesk">
+                                    <figure className="assortmentItem">
+                                        <img src={nameNecklace} alt="" className="assortmentImg"/>
+                                    </figure>
+                                </div>
+                            </div>
+                            <div className="assortmentColumn3">
+                                <div className="assortmentColumnDesk">
+                                    <figure className="assortmentItem">
+                                        <img src={earring} alt="" className="assortmentImg"/>
+                                    </figure>
+                                    <p className="assortmentInfo">
+                                        Earrings
+                                    </p>
+                                </div>
+                                <div className="assortmentColumnDesk">
+                                    <figure className="assortmentItem">
+                                        <img src={bracelet} alt="" className="assortmentImg"/>
+                                    </figure>
+                                    <p className="assortmentInfo">
+                                        Bracelets
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <section className="new">
+                            <h2 className="newTitle">New jewelery</h2>
+                            <div className="newJewelryRow">
+                                {newProduct.map((n) => (
+                                    <NewProduct key={n.id} data={n}/>
+                                ))}
+                            </div>
+                        </section>
+                    </div>
+                </main>
             </div>
-            <section className="new">
-              <h2 className="newTitle">New jewelery</h2>
-              <div className="newJewelryRow">
-                {newProduct.map((n) => (
-                  <NewProduct key={n.id} data={n} />
-                ))}
-              </div>
-            </section>
-          </div>
-        </main>
-      </div>
-    </Wrapper>
-  );
+        </Wrapper>
+    );
 }
 
 export default Home;
