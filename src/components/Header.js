41561e9f402e9b04b47de6c6a0150646f0676e3c
unmatched0 <hovhannisya03@mail.ru>
2022-12-07 20:27:38 +0400

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,12 +21,12 @@ function Header() {
           <Navbar.Brand href="/home">
             <figure className="logo">
               <Link to="/" className="logoLink">
-                <img src={logo} alt="nreni" className="logoImg" />
+                <img src={logo} alt="nreni" className="logoImg"/>
                 <p className="logoName">NRENI</p>
               </Link>
             </figure>
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
           <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
             <Nav>
               <ul className="navBlock">
@@ -43,36 +43,37 @@ function Header() {
                   <NavLink to="/contact" className="navLink">Contact</NavLink>
                 </li>
                 <li className="navList">
+                  <NavLink to="/cart" className="navLink">
+                    <img className="navImg" src={cart} alt=""/>
+                  </NavLink>
+                </li>
+                <li className="navList">
                   {sessionStorage.getItem('token')
                     ? (
                       <NavLink to="/profile" className="navLink">
-                        <img className="navImg" src={login} alt="" />
+                        <img className="navImg" src={login} alt=""/>
                       </NavLink>
                     )
                     : (
                       <NavLink to="/login" className="navLink">
-                        <img className="navImg" src={login} alt="" />
+                        <img className="navImg" src={login} alt=""/>
                       </NavLink>
                     )}
                 </li>
-                <li className="navList">
-                  <NavLink to="/cart" className="navLink">
-                    <img className="navImg" src={cart} alt="" />
-                  </NavLink>
-                </li>
+
                 <li className="navList">
                   <NavDropdown title="Languages" id="basic-nav-dropdown">
                     <ul className="subMenu">
                       <li className="subList" onClick={() => handleLangChange('us')}>
-                        <img className="subLanguageIcons" src={us} alt="" />
+                        <img className="subLanguageIcons" src={us} alt=""/>
                         <p className="subLangName">English</p>
                       </li>
                       <li className="subList" onClick={() => handleLangChange('arm')}>
-                        <img className="subLanguageIcons" src={arm} alt="" />
+                        <img className="subLanguageIcons" src={arm} alt=""/>
                         <p className="subLangName">Հայերեն</p>
                       </li>
                       <li className="subList" onClick={() => handleLangChange('ru')}>
-                        <img className="subLanguageIcons" src={russian} alt="" />
+                        <img className="subLanguageIcons" src={russian} alt=""/>
                         <p className="subLangName">Русский</p>
                       </li>
                     </ul>
@@ -86,4 +87,5 @@ function Header() {
     </header>
   );
 }
+
 export default Header;
