50579d1b81f7f59a563387f079ca4e851aa11f16
codeUser2000 <aidahakobyanf12@gmail.com>
2022-12-07 20:18:40 +0400

    react bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,8 @@
 import React, { useCallback } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import {
+  Container, Nav, Navbar, NavDropdown,
+} from 'react-bootstrap';
 import logo from '../assets/img/logo/logo.png';
 import login from '../assets/img/site/user.png';
 import cart from '../assets/img/site/cart.png';
@@ -13,67 +16,73 @@ function Header() {
   }, []);
   return (
     <header className="header">
-      <div className="container">
-        <div className="row">
-          <figure className="logo">
-            <Link to="/" className="logoLink">
-              <img src={logo} alt="nreni" className="logoImg" />
-              <p className="logoName">NRENI</p>
-            </Link>
-          </figure>
-          <nav className="nav">
-            <ul className="navBlock">
-              <li className="navList">
-                <NavLink to="/home" className="navLink">Home</NavLink>
-              </li>
-              <li className="navList">
-                <NavLink to="/shop" className="navLink">Shop</NavLink>
-              </li>
-              <li className="navList">
-                <NavLink to="/about" className="navLink">About</NavLink>
-              </li>
-              <li className="navList">
-                <NavLink to="/contact" className="navLink">Contact</NavLink>
-              </li>
-              <li className="navList">
-                {sessionStorage.getItem('token')
-                  ? (
-                    <NavLink to="/profile" className="navLink">
-                      <img className="navImg" src={login} alt="" />
-                    </NavLink>
-                  )
-                  : (
-                    <NavLink to="/login" className="navLink">
-                      <img className="navImg" src={login} alt="" />
-                    </NavLink>
-                  )}
-              </li>
-              <li className="navList">
-                <NavLink to="/cart" className="navLink">
-                  <img className="navImg" src={cart} alt="" />
-                </NavLink>
-              </li>
-              <li className="navList">
-                <img className="languageIcons" src={us} alt="" />
-                <ul className="subMenu">
-                  <li className="subList" onClick={() => handleLangChange('us')}>
-                    <img className="subLanguageIcons" src={us} alt="" />
-                    <p className="subLangName">English</p>
-                  </li>
-                  <li className="subList" onClick={() => handleLangChange('arm')}>
-                    <img className="subLanguageIcons" src={arm} alt="" />
-                    <p className="subLangName">Հայերեն</p>
-                  </li>
-                  <li className="subList" onClick={() => handleLangChange('ru')}>
-                    <img className="subLanguageIcons" src={russian} alt="" />
-                    <p className="subLangName">Русский</p>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/home">
+            <figure className="logo">
+              <Link to="/" className="logoLink">
+                <img src={logo} alt="nreni" className="logoImg" />
+                <p className="logoName">NRENI</p>
+              </Link>
+            </figure>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+            <Nav>
+              <ul className="navBlock">
+                <li className="navList">
+                  <NavLink to="/home" className="navLink">Home</NavLink>
+                </li>
+                <li className="navList">
+                  <NavLink to="/shop" className="navLink">Shop</NavLink>
+                </li>
+                <li className="navList">
+                  <NavLink to="/about" className="navLink">About</NavLink>
+                </li>
+                <li className="navList">
+                  <NavLink to="/contact" className="navLink">Contact</NavLink>
+                </li>
+                <li className="navList">
+                  {sessionStorage.getItem('token')
+                    ? (
+                      <NavLink to="/profile" className="navLink">
+                        <img className="navImg" src={login} alt="" />
+                      </NavLink>
+                    )
+                    : (
+                      <NavLink to="/login" className="navLink">
+                        <img className="navImg" src={login} alt="" />
+                      </NavLink>
+                    )}
+                </li>
+                <li className="navList">
+                  <NavLink to="/cart" className="navLink">
+                    <img className="navImg" src={cart} alt="" />
+                  </NavLink>
+                </li>
+                <li className="navList">
+                  <NavDropdown title="Languages" id="basic-nav-dropdown">
+                    <ul className="subMenu">
+                      <li className="subList" onClick={() => handleLangChange('us')}>
+                        <img className="subLanguageIcons" src={us} alt="" />
+                        <p className="subLangName">English</p>
+                      </li>
+                      <li className="subList" onClick={() => handleLangChange('arm')}>
+                        <img className="subLanguageIcons" src={arm} alt="" />
+                        <p className="subLangName">Հայերեն</p>
+                      </li>
+                      <li className="subList" onClick={() => handleLangChange('ru')}>
+                        <img className="subLanguageIcons" src={russian} alt="" />
+                        <p className="subLangName">Русский</p>
+                      </li>
+                    </ul>
+                  </NavDropdown>
+                </li>
+              </ul>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Provider} from 'react-redux';
-import {applyMiddleware, compose, legacy_createStore as createStore} from 'redux';
+import { Provider } from 'react-redux';
+import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux';
 import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 import App from './App';
@@ -9,8 +9,9 @@ import reportWebVitals from './reportWebVitals';
 import reducers from './store/reducers';
 import sagas from './store/sagas';
 import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-datepicker/dist/react-datepicker.css';
-import 'react-phone-number-input/style.css'
+import 'react-phone-number-input/style.css';
 import './assets/css/style.css';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -24,9 +25,9 @@ sagaMiddleware.run(sagas);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
 
 reportWebVitals();
