d6db532332c48a0d8d748037562fb5780239c643
unmatched0 <hovhannisya03@mail.ru>
2022-12-10 18:00:22 +0400

    change banner

--- src/components/MyCarouselComponent.js
+++ src/components/MyCarouselComponent.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import banner from '../assets/img/post/banner.jpeg';
+import banner from '../assets/img/post/banner.jpeg'
 
-function MyCarouselComp() {
+function MyCarouselComponent() {
   return (
     <div className="bannerRow">
       <figure className="bannerFigure">
         <img className="bannerImg" src={banner} alt="" />
       </figure>
       <div className="bannerDesk">
-        <h1 className="bannerTitle">SILVER NRENI</h1>
         <p className="bannerInfo">
-          Jewelry is a very personal thing... It should tell a story about the person who's wearing it!
-          We sell aesthetic and stylish jewelry. The most suitable gifts here for your loved ones
+            The best ARMENIAN silver you can get
         </p>
         <button className="bannerBtn">
           <Link className="bannerLink" to="/contact">
-            contact us
+            Shop now
           </Link>
         </button>
       </div>
@@ -24,4 +22,4 @@ function MyCarouselComp() {
   );
 }
 
-export default MyCarouselComp;
+export default MyCarouselComponent;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import Carousel from 'nuka-carousel';
-import { Link } from 'react-router-dom';
 import Wrapper from '../components/Wrapper';
 import MyCarouselComp from '../components/MyCarouselComp';
-import img from '../assets/img/post/banner.jpeg';
-import ring from '../assets/img/post/ring.jpg';
-import chainRing from '../assets/img/post/chainRing.jpg';
-import necklace from '../assets/img/post/necklace.jpg';
-import nameNecklace from '../assets/img/post/nameNecklace.jpg';
-import earring from '../assets/img/post/earring.jpg';
-import bracelet from '../assets/img/post/bracelet.jpg';
 import NewProduct from '../components/NewProduct';
 import newProduct from '../newProduct';
 import Blockquote from '../components/Blockquote';
+import Assortment from '../components/Assortment';
+import MyCarouselComponent from '../components/MyCarouselComponent';
 
 function Home() {
   return (
@@ -22,80 +16,24 @@ function Home() {
         <title>NRENI</title>
       </Helmet>
       <div id="header-carousel" className="carousel slide" data-ride="carousel">
-        <div className="carousel-inner">
-          <Carousel
-            autoplay="true"
-            dragging="true"
-            pauseOnHover="true"
-            wrapAround="true"
-          >
-            <MyCarouselComp imgSrc={img} />
-            <MyCarouselComp imgSrc={img} />
-          </Carousel>
-        </div>
+        <section className="banner">
+          <div className="container">
+            <div className="carousel-inner">
+              <Carousel
+                autoplay="true"
+                dragging="true"
+                pauseOnHover="true"
+                wrapAround="true"
+              >
+                <MyCarouselComp />
+                <MyCarouselComponent />
+              </Carousel>
+            </div>
+          </div>
+        </section>
         <main className="home">
           <div className="container">
-            <div className="assortment">
-              <div className="assortmentColumn1">
-                <div className="assortmentColumnDesk">
-                  <figure className="assortmentItem">
-                    <img
-                      style={{ objectPosition: 'bottom' }}
-                      src={ring}
-                      alt=""
-                      className="assortmentImg"
-                    />
-                  </figure>
-                </div>
-                <div className="assortmentColumnDesk">
-                  <p className="assortmentInfo">Rings</p>
-                  <figure className="assortmentItem">
-                    <img src={chainRing} alt="" className="assortmentImg" />
-                  </figure>
-                </div>
-              </div>
-              <div className="assortmentColumn2">
-                <div className="assortmentColumnDesk">
-                  <figure className="assortmentItem">
-                    <img
-                      style={{ objectPosition: 'bottom' }}
-                      src={necklace}
-                      alt=""
-                      className="assortmentImg"
-                    />
-                  </figure>
-                  <div className="assortmentDesk">
-                    <p className="assortmentInfo">Necklaces</p>
-                    <button type="button" className="assortmentBtn">
-                      <Link to="/shop" className="assortmentLink">Shop now</Link>
-                    </button>
-                  </div>
-                </div>
-                <div className="assortmentColumnDesk">
-                  <figure className="assortmentItem">
-                    <img src={nameNecklace} alt="" className="assortmentImg" />
-                  </figure>
-                </div>
-              </div>
-              <div className="assortmentColumn3">
-                <div className="assortmentColumnDesk">
-                  <figure className="assortmentItem">
-                    <img src={earring} alt="" className="assortmentImg" />
-                  </figure>
-                  <p className="assortmentInfo">
-                    Earrings
-                  </p>
-                </div>
-                <div className="assortmentColumnDesk">
-                  <figure className="assortmentItem">
-                    <img src={bracelet} alt="" className="assortmentImg" />
-                  </figure>
-                  <p className="assortmentInfo">
-                    Bracelets
-                  </p>
-                </div>
-              </div>
-            </div>
+            <Assortment />
             <section className="new">
               <h2 className="newTitle">New jewelery</h2>
               <div className="newJewelryRow">
