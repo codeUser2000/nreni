5303999ff1e99679e02dd0e562869e009c9d2cf2
unmatched0 <hovhannisya03@mail.ru>
2022-12-14 00:51:58 +0400

    change admin

--- src/components/AdminCreateProduct.js
+++ src/components/AdminCreateProduct.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
 import img from '../assets/img/post/chainRing.jpg';
+import img1 from '../assets/img/post/nameNecklace.jpg';
+import img2 from '../assets/img/post/bracelet.jpg';
 
 function AdminCreateProduct() {
   return (
@@ -21,6 +23,58 @@ function AdminCreateProduct() {
           </tr>
         </thead>
         <tbody className="adminTableTbody">
+          <tr>
+            <td>
+              <figure className="adminTableItem">
+                <img src={img1} alt="" className="adminTableImg" />
+              </figure>
+            </td>
+            <td>
+              <p className="adminTableName">name necklace</p>
+            </td>
+            <td>
+              <p className="adminTableDesc">Lorem Ipsum is simply dummy text.</p>
+            </td>
+            <td>
+              <p className="adminTableCategory">necklaces</p>
+            </td>
+            <td>
+              <p className="adminTablePrice">$14.00</p>
+            </td>
+            <td>
+              <div className="adminTableBtnRow">
+                <button className="adminTableView">View</button>
+                <button className="adminTableDelete">delete</button>
+                <button className="adminTableUpdate">update</button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <figure className="adminTableItem">
+                <img src={img2} alt="" className="adminTableImg" />
+              </figure>
+            </td>
+            <td>
+              <p className="adminTableName">bracelet</p>
+            </td>
+            <td>
+              <p className="adminTableDesc">Lorem Ipsum is simply dummy text.</p>
+            </td>
+            <td>
+              <p className="adminTableCategory">bracelets</p>
+            </td>
+            <td>
+              <p className="adminTablePrice">$28.00</p>
+            </td>
+            <td>
+              <div className="adminTableBtnRow">
+                <button className="adminTableView">View</button>
+                <button className="adminTableDelete">delete</button>
+                <button className="adminTableUpdate">update</button>
+              </div>
+            </td>
+          </tr>
           <tr>
             <td>
               <figure className="adminTableItem">
