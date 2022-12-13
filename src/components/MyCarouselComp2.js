0bf8523e4043a33f0c51ae0e0040354c3eda2054
unmatched0 <hovhannisya03@mail.ru>
2022-12-13 15:11:35 +0400

    change

--- src/components/MyCarouselComp2.js
+++ src/components/MyCarouselComp2.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import banner from '../assets/img/post/banner.jpeg';
 
-function MyCarouselComp2() {
+function MyCarouselComp2({ data }) {
   return (
-    <div className="bannerRow">
-      <figure className="bannerFigure">
-        <img className="bannerImg" src={banner} alt="" />
-      </figure>
-      <div className="bannerDesk">
+    <div className="banner2Row">
+      <div className="banner2Desk">
         <p className="banner2Info">
-          The best ARMENIAN silver you can get
+          {data}
         </p>
-        <button className="bannerBtn">
-          <Link className="bannerLink" to="/contact">
+        <button className="banner2Btn">
+          <Link className="banner2Link" to="/shop">
             Shop now
           </Link>
         </button>
       </div>
+      <figure className="banner2Figure">
+        <img className="banner2Img" src={banner} alt="" />
+      </figure>
     </div>
   );
 }
