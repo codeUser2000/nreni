20a44a44eea70418ab967be52f32b3b97fd699b6
unmatched0 <hovhannisya03@mail.ru>
2023-02-08 18:14:34 +0400

    media menu

--- src/components/NavBarMenu.js
+++ src/components/NavBarMenu.js
@@ -14,7 +14,6 @@ function NavBarMenu(props) {
         </figure>
         <nav className="nav">
           <ul className="navBlock">
-            <li>Aid</li>
             <li className="navList">
               <NavLink to="/home" className="navLink">Home</NavLink>
             </li>
